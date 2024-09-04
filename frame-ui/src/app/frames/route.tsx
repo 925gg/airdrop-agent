a63b242729902bfc12d579654602fadaa37486c7
Tung Pham <phamminhtung91@gmail.com>
2024-09-04 13:50:43 +0700

    add service placeholder

--- frame-ui/src/app/frames/route.tsx
+++ frame-ui/src/app/frames/route.tsx
@@ -2,8 +2,22 @@
 import { Button } from "frames.js/next";
 import { frames } from "./frames";
 import { TopicEnum } from "./constants";
+import { getRemainingDraws } from "../services/fortune-draw";
 
 const handleRequest = frames(async (ctx) => {
+  const remainingDraws = await getRemainingDraws();
+
+  if (!remainingDraws) {
+    return {
+      image: <span>No remaining draws</span>,
+      buttons: [
+        <Button action="post" target="/">
+          OK
+        </Button>,
+      ],
+    };
+  }
+
   return {
     image: <span>Select a topic</span>,
     buttons: [
---
+++ frame-ui/src/app/services/fortune-draw.ts
@@ -0,0 +1,3 @@
+export const getRemainingDraws = async () => {
+  return 0;
+};
