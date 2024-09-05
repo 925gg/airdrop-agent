69722bf0408b52f1b999b869e3eacbb35f5a6cb2
Thieund <thieu@925.gg>
2024-09-05 16:23:47 +0700

    Update generate-airdrop-signature.ts

--- frame-ui/src/app/services/generate-airdrop-signature.ts
+++ frame-ui/src/app/services/generate-airdrop-signature.ts
@@ -1,15 +1,20 @@
+import dayjs from "dayjs";
 import { getBytes, keccak256, toBeHex, toUtf8Bytes, Wallet } from "ethers";
 
 export const generateSignature = async (
   transactionId: number,
   wallet: string
 ) => {
-  const message = `${toBeHex(wallet, 20)}/0/${transactionId}`;
+  const expiredAt = dayjs().add(5, "m").unix();
+  const message = `to:${toBeHex(
+    wallet,
+    20
+  )}/typeId:0/transactionId:${transactionId}/expiredAt:${expiredAt}`;
   const hash = keccak256(toUtf8Bytes(message));
 
   const verifier = new Wallet(
     process.env.CHIBI_BATTLE_ITEM_MINT_FROM_CHEST_PRIVATE_KE ?? ""
   );
   const signature = await verifier.signMessage(getBytes(hash));
-  return signature;
+  return { signature, expiredAt };
 };
